72810df8a6158ff21a73e83cb80d89a3325d7bde
Sandip Das <63204939+Sandip-Rocks@users.noreply.github.com>
2021-05-25 06:06:24 +0530

    Update corona.java

--- corona.java
+++ corona.java
@@ -5,20 +5,19 @@ import java.io.InputStreamReader;
 
 class corona {
 
-	static boolean report(String tmp1, String tmp2,
-								int m, int n)
+	static boolean report(String tmp1, String tmp2,int m, int n)
 	
 	{
 		int j = 0;
 		for (int i = 0; i < n && j < m; i++)
 		{
-			if (tmp1.charAt(j) == tmp2.charAt(i)) //iterate all the characters
+			if (tmp1.charAt(j) == tmp2.charAt(i))
 			{
 				j++;
 
 			}
 	}
-		return (j == m); //return true or false
+		return (j == m);
 	}
 
 	public static void main(String[] args)throws IOException
@@ -29,7 +28,7 @@ class corona {
 		String str1=in.readLine();
 		System.out.println("The name of the Virus is "+str1);
 		System.out.println("Enter number of people");
-		int n,i,len1,len2;
+		int n,i;
 		
 		String str2;
 		n=Integer.parseInt(in.readLine()); //input no of people
@@ -40,28 +39,18 @@ class corona {
 			str2=in.readLine();
 			arr[i]=str2;
 		}
-		for(i=0;i<arr.length;i++) { //iterate all the strings of the array
-			String temp=arr[i];
-			System.out.println("Temp "+temp);
-			len1=str1.length();
-			len2=temp.length();
-			boolean result = report(str1, temp, len1, len2); //function call to receive boolean value
-			if (result)
-				System.out.println("POSITIVE");
-			else
-				System.out.println("NEGATIVE");
+		for( i=0;i<arr.length;i++) {
+			String tmp1 = arr[i];
+	        int length1 = tmp1.length();
+	        int length2 = str1.length();
+	        boolean result = report(tmp1, str1, length1, length2);
+	 
+	        if (result)
+			System.out.println("POSITIVE");
+		else
+			System.out.println("NEGATIVE");
 		}
 
-//		String str1 = "crns";
-//        String str2 = "coronavirus";
-//        int len1 = str1.length();
-//        int len2 = str2.length();
-//        boolean result = report(str1, str2, len1, len2);
-// 
-//        if (result)
-//		System.out.println("POSITIVE");
-//	else
-//		System.out.println("NEGATIVE");
-	} //End of public
-} //End of class
+	}
+}
 
