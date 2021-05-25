e5c52822f4aafd687d8fd2a660dd06128d5cb3f7
Sandip Das <63204939+Sandip-Rocks@users.noreply.github.com>
2021-05-25 06:27:45 +0530

    Update corona.java

--- corona.java
+++ corona.java
@@ -5,7 +5,8 @@ import java.io.InputStreamReader;
 
 class corona {
 
-	static boolean report(String tmp1, String tmp2,int m, int n)
+	static boolean report(String tmp1, String tmp2,
+								int m, int n)
 	
 	{
 		int j = 0;
@@ -26,6 +27,7 @@ class corona {
 		BufferedReader in=new BufferedReader(read);
 		System.out.println("Enter Virus Name");
 		String str1=in.readLine();
+		str1=str1.toLowerCase(); //convert String into Lower Case
 		System.out.println("The name of the Virus is "+str1);
 		System.out.println("Enter number of people");
 		int n,i;
@@ -33,24 +35,34 @@ class corona {
 		String str2;
 		n=Integer.parseInt(in.readLine()); //input no of people
 		String arr[]=new String[n]; //Declare String of array with size= no of people
-		System.out.println("The total no of people is "+n);
-		for(i=0;i<n;i++) {
-			System.out.println("Enter the Sample report of person "+(i+1));
-			str2=in.readLine();
-			arr[i]=str2;
-		}
-		for( i=0;i<arr.length;i++) {
-			String tmp1 = arr[i];
-	        int length1 = tmp1.length();
-	        int length2 = str1.length();
-	        boolean result = report(tmp1, str1, length1, length2);
-	 
-	        if (result)
-			System.out.println("POSITIVE");
-		else
-			System.out.println("NEGATIVE");
-		}
+		while(true) { //Infinite loop
+			if(n>=1 && n<=10) { //condition to validate the whenther n is between 1 to 10
+				System.out.println("The total no of people is "+n);
+				
+			}
+			else {
+				System.out.println("Enter number between 1 to 10");
+				n=Integer.parseInt(in.readLine());
+			}
+			for(i=0;i<n;i++) {
+				System.out.println("Enter the Sample report of person "+(i+1));
+				str2=in.readLine();
+				arr[i]=str2;
+			}
+			for( i=0;i<arr.length;i++) {
+				String tmp1 = arr[i];
+		        int length1 = tmp1.length();
+		        int length2 = str1.length();
+		        boolean result = report(tmp1, str1, length1, length2);
+		 
+		        if (result)
+				System.out.println("POSITIVE");
+			else
+				System.out.println("NEGATIVE");
+			}
+			break;
 
+		}
+		
 	}
 }
-
